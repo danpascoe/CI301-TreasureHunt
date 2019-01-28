c5cb4fad6dd51734b47d9438386b59c15ccb2692
DanPascoe <danpascoed@gmail.com>
2019-01-28 23:04:42 +0000

    Add button & Intent to MainActivity
    The button will change the app layout to the MapActivity once created

--- Application/app/src/main/java/application/pascoe/com/ci301/MainActivity.java
+++ Application/app/src/main/java/application/pascoe/com/ci301/MainActivity.java
@@ -1,7 +1,10 @@
 package application.pascoe.com.ci301;
 
+        import android.content.Intent;
         import android.support.v7.app.AppCompatActivity;
         import android.os.Bundle;
+        import android.view.View;
+        import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button = findViewById(R.id.btn_start);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                    //CHANGE TO MAPS ACTIVITY
+                    Intent intent = new Intent(MainActivity.this, MainActivity.class);
+                    startActivity(intent);
+                }
+        });
     }
 }
