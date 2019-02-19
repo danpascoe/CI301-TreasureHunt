b4ad7ed4b6d61370f47eeec9b8e0d8c2d41d280a
DanPascoe <danpascoed@gmail.com>
2019-02-19 19:18:42 +0000

    REVERT

--- Application/app/src/main/java/application/pascoe/com/ci301/gameplay/Gameplay.java
+++ Application/app/src/main/java/application/pascoe/com/ci301/gameplay/Gameplay.java
@@ -5,51 +5,26 @@ package application.pascoe.com.ci301.gameplay;
         import com.google.android.gms.maps.model.LatLng;
         import com.google.maps.android.SphericalUtil;
         import application.pascoe.com.ci301.constants.Constants;
-        import application.pascoe.com.ci301.sqlite.SQLManager;
+        import application.pascoe.com.ci301.sqlite.GameplaySQLManager;
 
 public class Gameplay {
 
-    public static LatLng[] LatLngList;
-    public static LatLng[] clueLocations = {new LatLng(50.871909,0.574404)
-                                            ,new LatLng(50.867859,0.573043)
-                                            ,new LatLng(50.868561,0.576427)
-                                            ,new LatLng(50.869317,0.578672)
-                                            ,new LatLng(50.8700337,0.579594)};
+    public static LatLng[] LatLngArr;
+    public static int[] positionID;
 
-    private static String[] clues = {"Outside Kirsty's House", "Outside Gran's House", "Primary School", "Keppel Road Bus Stop", "Home", "OUT OF RANGE CLUE FOR DEBUG", "YOU DID IT!"};
-    private static int currentClue = 0;
-    private static SQLManager SQLManager;
+    private static GameplaySQLManager sqlManager;
 
-    public static int GameInitiate(Context context){
-
-        SQLManager = SQLManager.getInstance(context);
-
-        getPositionList();
-
-        return clueLocations.length;
+    public static void GameInitiate(Context context){
+        getGameInfo();
     }
 
-    public static boolean checkDistance(LatLng currentPos){
-        double distanceCheck = SphericalUtil.computeDistanceBetween(currentPos, clueLocations[currentClue]);
-        if(distanceCheck <= Constants.MIN_PLAYER_TO_CLUE_DISTANCE){
-            currentClue++;
-            return true;
-        }else {
-            return false;
-        }
+    public static void getGameInfo(){
+        LatLngArr = sqlManager.getLatLng("default");
+        return;
     }
 
-    public static void getPositionList(){
-
+    public boolean checkDistance(){
+        return false;
     }
-
-
-    public static String[] getClue() {
-        String currentClueText = Integer.toString(currentClue + 1);
-        String[] arrClue = {currentClueText, clues[currentClue]};
-        return arrClue;
-    }
-
-
 
 }
